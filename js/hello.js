1517c8b899880aaa1f1780eea951feb95204e11d
taknarae <dddddum@naver.com>
2024-09-24 14:25:09 +0900

    240924

---
+++ js/hello.js
@@ -0,0 +1,13 @@
+// console.log('Hello');
+// (-tm) node js/hello.js
+
+document.getElementById('date').innerHTML = Date();
+document.getElementById('string').innerHTML = "Hello!!!!!!!";
+
+var userId = 1;
+var userName = "Kim";
+// console.log(userId, '-----', userName);
+
+"use strict"; //엄격모드
+for(var i=0; i < 5; ++i){}
+// console.log(i);
